21cb52d3c8bdd0467ef98303c955cb589b7af86d
David <david.ezequiel.perez@gmail.com>
2023-08-09 15:58:04 -0300

    Form and Patients objects build.

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,24 @@
+
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white shadow-md  px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+            <span className="font-normal normal-case">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+            <span className="font-normal normal-case">Juan</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+            <span className="font-normal normal-case">juan@email.com</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''}
+            <span className="font-normal normal-case">10 Diciembre 2022</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {''}
+            <span className="font-normal normal-case">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus error molestias, accusantium velit culpa quos ducimus nisi beatae neque fugiat. Hic placeat necessitatibus mollitia explicabo sunt aliquid soluta harum possimus!</span>
+        </p>
+    </div>
+  )
+}
+
+export default Paciente
