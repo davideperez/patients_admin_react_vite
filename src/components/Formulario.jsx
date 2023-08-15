cd6c0f7855640bef51990bd2ea08bee04f0723eb
David <david.ezequiel.perez@gmail.com>
2023-08-15 16:42:12 -0300

    Conditional text on a form. Button displays Agregar paciente o Editar paciente respectevly.

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -19,6 +19,7 @@ const Formulario = ({ pacientes, setPacientes, paciente }) => {
             setFecha(paciente.fecha)
             setSintomas(paciente.sintomas)
         }
+
     },[paciente])
     
     const generarId = () => {
@@ -143,7 +144,7 @@ const Formulario = ({ pacientes, setPacientes, paciente }) => {
                 <input 
                     type="submit"
                     className=" bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all"
-                    value='Agregar Paciente' />
+                    value= {paciente.id ? 'Editar Paciente': 'Agregar Paciente'} />
             </form>
 
         </div>
